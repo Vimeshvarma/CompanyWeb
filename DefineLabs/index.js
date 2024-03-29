4eb7785ba3d7c74c5a5263c2ee457d38c86af4f2
Vimeshvarma <113052902+Vimeshvarma@users.noreply.github.com>
2024-03-29 17:12:40 +0530

    Add files via upload

---
+++ DefineLabs/index.js
@@ -0,0 +1,6 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("aboutDropdownContent");
+    var arrow = document.getElementById("aboutArrow");
+    dropdown.classList.toggle("active");
+    arrow.classList.toggle("active");
+}
